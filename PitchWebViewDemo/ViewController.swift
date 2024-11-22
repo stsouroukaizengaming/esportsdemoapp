5a059de7fca71b893cce810e139953c06e246511
s.tsourou <s.tsourou@kaizengaming.com>
2024-11-22 14:22:24 +0200

    First commit

--- PitchWebViewDemo/ViewController.swift
+++ PitchWebViewDemo/ViewController.swift
@@ -6,12 +6,19 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var webView: WKWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        if let url = URL(string: "https://disir.oddin.gg/csgo/scoreboard?brandToken=b1248112-ccd9-4908-a9fd-acedb48d2c54&darkMode=true&id=bWF0Y2gvb2Q6bWF0Y2g6MTMyMjk4Mg%3D%3D&lang=en&layout=&t=4267634582&theme=dark") {
+            let request = URLRequest(url: url)
+            webView.load(request)
+        }
     }
 
 
